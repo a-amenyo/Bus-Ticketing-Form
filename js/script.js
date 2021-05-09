2b6406941e8f029071ae51ba7ee28bbca7e85bdc
yoooel <72613221+yoooel@users.noreply.github.com>
2021-05-09 13:27:05 +0000

    validations update

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ document.getElementById("submit").addEventListener("click", (event) => {
     let fullName = document.getElementById("fName").value;
     let date = document.getElementById("date").value;
     let people = document.getElementById("num-people").value;
-    let message = document.querySelector("textarea").value;
+    let message = document.querySelector("textarea[name='comment']").value;
     let oneWay = document.getElementById("oneway");
     let roundTrip = document.getElementById("roundtrip");
-
+   
 
     // Validations 
     let trip;
@@ -19,7 +19,12 @@ document.getElementById("submit").addEventListener("click", (event) => {
 
     if (fullName == "") {
         document.getElementById("fName").style.borderColor = 'red';
+        alert("Please enter your name ");
     }
+    else if (!(roundTrip.checked) && !(oneWay.checked))  {
+        alert("Please select your preferred trip type");
+    }
+  
     else if (date == "") {
         document.getElementById("date").style.borderColor = 'red';
         alert(" Please choose a valid date");
@@ -27,40 +32,35 @@ document.getElementById("submit").addEventListener("click", (event) => {
     }
     else if (people == "") {
         document.getElementById("num-people").style.borderColor = 'red';
+        alert("Please select number of people you are booking for ");
+    }
+    else if (people < 1 ){
+        document.getElementById("num-people").style.borderColor = 'red';
+        alert("You cannot book for less than 1 person"); 
     }
 
-    else if (!(roundTrip.checked) && !(oneWay.checked)) {
-        alert("Please select your preferred trip type");
+    else if ( people >20){
+        document.getElementById("num-people").style.borderColor = 'red';
+        alert("You cannot book for more than 20 people")
     }
+
     else if (message == "") {
-        document.querySelector("textarea").style.borderColor = 'red';
+        alert("Please enter a message");
     }
+
+   
    
 // if everything checks 
 
-else {
-    alert (" Thank you !Form successfully submitted!")
-}
+    else {
+    
+            alert (" Thank you !Form successfully submitted!")
 
-    event.defaultPrevented(); 
+    
+    }
+
+    event.preventDefault(); 
 })
 
 
 
-// function fName(){
-//     let fullName = document.getElementById("fName").value;
-//     if (fullName != " "){
-//         console.log(fullName);
-//         return fName;
-
-//     }
-//     else{
-//         return null;
-//     }
-
-// }
-// let submit = document.getElementById("submit");
-// console.log(submit);
-// submit.addEventListener("click", function(){
-//     fName();
-// })
